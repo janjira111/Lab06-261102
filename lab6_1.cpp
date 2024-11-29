552693dd9309054c0aa478f652af2cb27813feda
janjira111 <janjira_thanach@cmu.ac.th>
2024-11-29 16:36:49 +0700

    lab6_1.cpp completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -2,8 +2,21 @@
 using namespace std;
 
 int main(){
-    cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+  int n=1;
+  int a = 0, b = 0;
+      
+    while(n!=0){
+      cout << "Enter an integer: ";
+      cin >> n;
+      if(n %2==0 && n != 0 ){
+        a++;
+      }
+      if(n %2 == 1 && n!= 0 ) {
+        b++;
+      }
+    }
+
+    cout << "#Even numbers = " << a << "\n";
+    cout << "#Odd numbers = " << b;
     return 0;
 }
